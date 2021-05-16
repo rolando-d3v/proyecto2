5a38305f1060d8a8979736f452fd9ba3fe0152ea
rolando-R <landon3388@gmail.com>
2021-05-16 16:21:05 -0500

    primer proyecto subido

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import * as HiIcons from "react-icons/hi";
+import * as MdIcons from "react-icons/md";
+import * as RiIcons from "react-icons/ri";
+import * as IoIcons from "react-icons/io5";
+
+function App() {
+  return (
+    <div>
+      <header className="header-youtube">
+        <nav className="nav-logo">
+          <HiIcons.HiOutlineMenu className="barra" />
+          <img
+            className="logo-youtube"
+            src="/assets/youtube.png"
+            alt="youtube"
+          />
+          <span>PE</span>
+        </nav>
+
+        <nav className="nav-search">
+          <div className="content-input">
+            <input type="text" placeholder="Buscar" />
+            <span>
+              <HiIcons.HiOutlineSearch />
+            </span>
+          </div>
+          <MdIcons.MdMic className="micro" />
+        </nav>
+
+        <nav className="nav-icons">
+          <div className="content-icons">
+
+              <RiIcons.RiVideoAddFill className='video' />
+
+              <RiIcons.RiGridFill className='icons' />
+
+            <div id='campana' >
+              <IoIcons.IoNotificationsSharp  />
+              <span className='numero' >7</span>
+            </div>
+
+          </div>
+
+          <img className="avatar" src="/assets/avatar2.jpg" alt="avatar" />
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './sass/normalize.css'
+import './sass/index.scss';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+
+    <App />
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
